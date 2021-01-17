f9afa567be2c25cc049d9511b5126f584906bdd6
wujianchang <wujianchang@xiaoice.com>
2021-01-17 21:20:44 +0800

    crawl baidu

--- scrapy_tutorial/spiders/first_spider.py
+++ scrapy_tutorial/spiders/first_spider.py
@@ -1,10 +1,13 @@
 import scrapy
+from bs4 import BeautifulSoup as bs
 
 
 class FirstSpiderSpider(scrapy.Spider):
     name = 'first_spider'
     allowed_domains = ['baidu.com']
-    start_urls = ['http://baidu.com/']
+    start_urls = ['https://baidu.com/s?q2=红棉小冰']
 
     def parse(self, response):
-        pass
+        soup = bs(response.text, 'lxml')
+        for search_result in soup.find_all(class_='result c-container new-pmd'):
+            yield search_result.find(class_='t').get_text()
