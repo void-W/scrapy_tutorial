bb7a2ef0feeec7ce7d646b02de0113fc62cdbfc9
wujianchang <wujianchang@xiaoice.com>
2021-01-22 14:44:31 +0800

    add header

--- scrapy_tutorial/spiders/first_spider.py
+++ scrapy_tutorial/spiders/first_spider.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup as bs
 class FirstSpiderSpider(scrapy.Spider):
     name = 'first_spider'
     allowed_domains = ['baidu.com']
-    start_urls = ['https://unsplash.com/s/photos/book']
+
+    def start_requests(self):
+        yield scrapy.Request("https://unsplash.com/s/photos/book", headers={"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75'})
 
     def parse(self, response):
         soup = bs(response.text, 'lxml')
@@ -14,7 +16,9 @@ class FirstSpiderSpider(scrapy.Spider):
         }
         for image in soup.find_all("img"):
             try:
-                item['image_urls'].append(image['src'])
+                url = image['src']
+                if "http" in url:
+                    item['image_urls'].append(url)
             except Exception:
                 pass
         return item
