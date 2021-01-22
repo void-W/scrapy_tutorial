5743ab42c1a97fed68b269c04c46b47d28236a3b
wujianchang <wujianchang@xiaoice.com>
2021-01-22 14:35:02 +0800

    image pipeline

--- scrapy_tutorial/settings.py
+++ scrapy_tutorial/settings.py
@@ -12,12 +12,14 @@ BOT_NAME = 'scrapy_tutorial'
 SPIDER_MODULES = ['scrapy_tutorial.spiders']
 NEWSPIDER_MODULE = 'scrapy_tutorial.spiders'
 
+IMAGES_STORE = r'.\image'
+ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrapy_tutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -37,34 +39,34 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'scrapy_tutorial.middlewares.ScrapyTutorialSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'scrapy_tutorial.middlewares.ScrapyTutorialDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'scrapy_tutorial.pipelines.ScrapyTutorialPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- scrapy_tutorial/spiders/first_spider.py
+++ scrapy_tutorial/spiders/first_spider.py
@@ -5,9 +5,16 @@ from bs4 import BeautifulSoup as bs
 class FirstSpiderSpider(scrapy.Spider):
     name = 'first_spider'
     allowed_domains = ['baidu.com']
-    start_urls = ['https://baidu.com/s?q2=红棉小冰']
+    start_urls = ['https://unsplash.com/s/photos/book']
 
     def parse(self, response):
         soup = bs(response.text, 'lxml')
-        for search_result in soup.find_all(class_='result c-container new-pmd'):
-            yield search_result.find(class_='t').get_text()
+        item = {
+            'image_urls': []
+        }
+        for image in soup.find_all("img"):
+            try:
+                item['image_urls'].append(image['src'])
+            except Exception:
+                pass
+        return item
